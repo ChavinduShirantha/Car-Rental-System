aa94f8e8894321f89a15b89c123c4b72e4be4044
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-17 17:23:49 +0530

    Added About Section

--- Front_End/assets/js/myScript.js
+++ Front_End/assets/js/myScript.js
@@ -31,3 +31,15 @@ function animateText() {
     }
 
 };
+
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    });
+}
+window.addEventListener('load', () => {
+    aos_init();
+});
