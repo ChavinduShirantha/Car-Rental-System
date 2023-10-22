a234d83cee9de80c8cbdedcbf398140e5e262028
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-22 11:21:18 +0530

    Added WebAppConfig & WebRootConfig Configuration Classes

---
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : Chavindu
+ * created : 10/22/2023-11:17 AM
+ **/
+@Configuration
+@EnableWebMvc
+public class WebAppConfig {
+}
---
+++ Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Chavindu
+ * created : 10/22/2023-11:18 AM
+ **/
+@Configuration
+public class WebRootConfig {
+}
