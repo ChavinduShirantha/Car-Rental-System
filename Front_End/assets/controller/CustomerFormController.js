014febba5f32e6f40339d32f2bcdcff5b7892ac9
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-25 15:47:36 +0530

    Updated Content

--- Front_End/assets/controller/CustomerFormController.js
+++ Front_End/assets/controller/CustomerFormController.js
@@ -1,6 +1,6 @@
 let baseURL = "http://localhost:8080/Back_End_war/";
 
-$("#btnSaveCustomerForm").click(function () {
+/*$("#btnSaveCustomerForm").click(function () {
     let customerFormData = new FormData($("#customerFormDetails")[0]);
     $.ajax({
         url: baseURL + "registerUser",
@@ -12,6 +12,38 @@ $("#btnSaveCustomerForm").click(function () {
             alert(res.message)
         },
         error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});*/
+
+$("#btnUpdateCustomerForm").click(function () {
+    let customerFormData = new FormData($("#customerFormDetails")[0]);
+    $.ajax({
+        url: baseURL + "registerUser/update",
+        method: "post",
+        data: customerFormData,
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            alert(res.message)
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
+
+$("#btnDeleteCustomerForm").click(function () {
+    let userId = $("#user_Id").val();
+    $.ajax({
+        url: baseURL + "registerUser?id=" + userId,
+        method: "delete",
+        dataType: "json",
+        success: function (res) {
+            alert(res.message)
+            getUserDetails();
+        }, error: function (error) {
             alert(error.responseJSON.message);
         }
     });
