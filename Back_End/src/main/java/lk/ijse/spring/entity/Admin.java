265927373fea7e328f33e3b08534b5e66afd069b
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-27 08:54:29 +0530

    Added Admin & AdminDTO classes

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * @author : Chavindu
+ * created : 10/27/2023-8:51 AM
+ **/
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Admin {
+    @Id
+    private String user_Id;
+    private String firstName;
+    private String lastName;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String nic_Img;
+    private String license_No;
+    private String license_Img;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+}
