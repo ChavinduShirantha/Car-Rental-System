a89a5fdc5e26d0de3fd326bc1aab7636498bbe1c
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-28 17:32:12 +0530

    Added RentRepo class

---
+++ Back_End/src/main/java/lk/ijse/spring/repo/RentRepo.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Rent;
+
+/**
+ * @author : Chavindu
+ * created : 10/28/2023-5:31 PM
+ **/
+
+public interface RentRepo extends JpaRepository<Rent,String>{
+}
