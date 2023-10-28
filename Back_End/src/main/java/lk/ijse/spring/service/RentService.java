0f870437734a22d9c0f0a21518de61cda124517f
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-28 17:33:58 +0530

    Added RentService & RentServiceImpl classes

---
+++ Back_End/src/main/java/lk/ijse/spring/service/RentService.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.service;
+
+/**
+ * @author : Chavindu
+ * created : 10/28/2023-5:33 PM
+ **/
+
+public interface RentService {
+}
---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.service.RentService;
+
+/**
+ * @author : Chavindu
+ * created : 10/28/2023-5:33 PM
+ **/
+
+public class RentServiceImpl implements RentService {
+}
