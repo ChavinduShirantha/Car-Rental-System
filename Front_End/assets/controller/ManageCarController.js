d41869dbd1bdd33256d5bcd345da492354ce4832
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-29 22:03:01 +0530

    Updated Content

--- Front_End/assets/controller/ManageCarController.js
+++ Front_End/assets/controller/ManageCarController.js
@@ -186,5 +186,30 @@ function clearInputFields() {
     $("#user_Id").focus();
 }
 
+function loadSelectedImage(divId) {
+
+    $(divId).on("change", function (e) {
+        let file = e.target.files;
+
+        if (FileReader && file && file.length) {
+            let reader = new FileReader();
+            reader.onload = function () {
+                $(divId).parent().children(":eq(0)").css({
+                    "background": `url(${reader.result})`,
+                    "background-size": "cover",
+                    "background-position": "center"
+                });
+            }
+            reader.readAsDataURL(file[0]);
+        }
+
+    });
+}
+
+loadSelectedImage("#front_view");
+loadSelectedImage("#back_view");
+loadSelectedImage("#side_view");
+loadSelectedImage("#interior_view");
+
 
 
