f51a50cb7411e569a043dcc673c3ca1dd30f35af
ChavinduShirantha <chavindushirantha@gmail.com>
2023-11-01 15:47:19 +0530

    Updated Content

--- Front_End/assets/controller/DashboardController.js
+++ Front_End/assets/controller/DashboardController.js
@@ -5,7 +5,7 @@ $("#btnSearchCarDetails").click(function () {
     let type = $("#type").val();
     let transmission_type = $("#transmission_type").val();
     let fuel_Type = $("#fuel_type").val();
-
+    $("#carDetailsPreview>div").remove();
     console.log(type);
     console.log(transmission_type);
     console.log(fuel_Type);
@@ -17,6 +17,19 @@ $("#btnSearchCarDetails").click(function () {
         dataType: "json",
         success: function (res) {
             console.log(res);
+            for (let i of res) {
+                $("#carDetailsPreview").append(
+                    `<div class="col-4">
+                        <div class="detailBox">
+                            <div class="detailBox-body">
+                                <h5 class="detailBox-title">${i.brand}</h5>
+                                <img src="${"../../../"+i.image.front_view}" class="d-block w-100" style="height: 300px" alt="">
+                            </div>
+                        </div>
+                    </div>`
+                )
+            }
+
         },
         error: function (error) {
             alert(error.responseJSON.message);
