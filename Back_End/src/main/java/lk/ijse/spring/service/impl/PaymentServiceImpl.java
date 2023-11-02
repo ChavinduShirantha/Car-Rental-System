734731bfcfbfe35fee7e99e5d509d914dc325586
ChavinduShirantha <chavindushirantha@gmail.com>
2023-11-02 12:26:30 +0530

    Updated Content

--- Back_End/src/main/java/lk/ijse/spring/service/impl/PaymentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/PaymentServiceImpl.java
@@ -1,16 +1,34 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.PaymentDTO;
+import lk.ijse.spring.entity.Admin;
+import lk.ijse.spring.entity.Payment;
+import lk.ijse.spring.repo.PaymentRepo;
 import lk.ijse.spring.service.PaymentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : Chavindu
  * created : 11/2/2023-12:21 PM
  **/
-
+@Service
+@Transactional
 public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    PaymentRepo repo;
+    @Autowired
+    ModelMapper mapper;
     @Override
     public void savePayment(PaymentDTO dto) {
+        Payment payment = mapper.map(dto, Payment.class);
+        if (repo.existsById(dto.getPayment_Id())) {
+            throw new RuntimeException(dto.getPayment_Id() + " is already available, please insert a new ID");
+        }
 
+        System.out.println(payment);
+        repo.save(payment);
     }
 }
