fd262508ab3f6b32174baa2e5c8f1b8ef72ebb56
ChavinduShirantha <chavindushirantha@gmail.com>
2023-11-02 14:17:26 +0530

    Updated Content

--- Back_End/src/main/java/lk/ijse/spring/repo/PaymentRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/PaymentRepo.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Chavindu
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  **/
 
 public interface PaymentRepo extends JpaRepository<Payment,String> {
+    @Query(value = "SELECT payment_Id FROM Payment ORDER BY payment_Id DESC LIMIT 1", nativeQuery = true)
+    String getLastID();
 }
